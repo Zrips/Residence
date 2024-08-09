0f6f98ac94c3077647123656e0d40670840fee34
Zrips <zripas@gmail.com>
2024-08-09 12:36:36 +0300

    Disabling boat leashing on 1.21+ servers

--- src/com/bekvon/bukkit/residence/listeners/ResidencePlayerListener1_21.java
+++ src/com/bekvon/bukkit/residence/listeners/ResidencePlayerListener1_21.java
@@ -1,6 +1,8 @@
 package com.bekvon.bukkit.residence.listeners;
 
 import org.bukkit.Location;
+import org.bukkit.entity.Boat;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
@@ -9,6 +11,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerSignOpenEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -16,9 +19,12 @@ import org.bukkit.potion.PotionEffectType;
 import com.bekvon.bukkit.residence.Residence;
 import com.bekvon.bukkit.residence.containers.Flags;
 import com.bekvon.bukkit.residence.containers.lm;
+import com.bekvon.bukkit.residence.protection.ClaimedResidence;
 import com.bekvon.bukkit.residence.protection.FlagPermissions;
 import com.bekvon.bukkit.residence.protection.FlagPermissions.FlagCombo;
 
+import net.Zrips.CMILib.Entities.CMIEntityType;
+import net.Zrips.CMILib.Logs.CMIDebug;
 import net.Zrips.CMILib.Version.Version;
 
 public class ResidencePlayerListener1_21 implements Listener {
@@ -50,5 +56,37 @@ public class ResidencePlayerListener1_21 implements Listener {
 
         // Removing weaving effect on death as there is no other way to properly handle this effect inside residence
         ent.removePotionEffect(PotionEffectType.WEAVING);
+    }
+
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    public void onFenceLeashInteract(PlayerInteractEntityEvent event) {
+
+        // Disabling listener if flag disabled globally
+        if (!Flags.leash.isGlobalyEnabled())
+            return;
+
+        // disabling event on world
+        if (plugin.isDisabledWorldListener(event.getRightClicked().getWorld()))
+            return;
+        Player player = event.getPlayer();
+
+        Entity entity = event.getRightClicked();
+
+        if (!(entity instanceof Boat))
+            return;
+
+        if (plugin.isResAdminOn(player))
+            return;
+
+        ClaimedResidence res = plugin.getResidenceManager().getByLoc(entity.getLocation());
+
+        if (res == null)
+            return;
+
+        if (res.getPermissions().playerHas(player, Flags.leash, FlagCombo.OnlyFalse)) {
+            plugin.msg(player, lm.Residence_FlagDeny, Flags.leash, res.getName());
+            event.setCancelled(true);
+            player.updateInventory();
+        }
     }
 }
