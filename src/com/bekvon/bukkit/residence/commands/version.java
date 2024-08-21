3568ad7d4de93ec969004650911fa38e47f4f1f7
Zrips <zripas@gmail.com>
2024-08-21 12:41:31 +0300

    Better version output

--- src/com/bekvon/bukkit/residence/commands/version.java
+++ src/com/bekvon/bukkit/residence/commands/version.java
@@ -2,10 +2,17 @@ package com.bekvon.bukkit.residence.commands;
 
 import java.util.Arrays;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 
+import net.Zrips.CMILib.Container.CMIText;
 import net.Zrips.CMILib.FileHandler.ConfigReader;
+import net.Zrips.CMILib.Locale.LC;
+import net.Zrips.CMILib.Messages.CMIMessages;
+import net.Zrips.CMILib.Version.Version;
+
 import com.bekvon.bukkit.residence.Residence;
 import com.bekvon.bukkit.residence.containers.CommandAnnotation;
 import com.bekvon.bukkit.residence.containers.cmd;
@@ -15,31 +22,51 @@ public class version implements cmd {
     @Override
     @CommandAnnotation(simple = false, priority = 5900)
     public Boolean perform(Residence plugin, CommandSender sender, String[] args, boolean resadmin) {
-	sender.sendMessage(ChatColor.GRAY + "------------------------------------");
-	sender.sendMessage(ChatColor.RED + "This server running " + ChatColor.GOLD + "Residence" + ChatColor.RED + " version: " + ChatColor.BLUE + plugin.getResidenceVersion());
-	sender.sendMessage(ChatColor.GREEN + "Created by: " + ChatColor.YELLOW + "bekvon");
-	sender.sendMessage(ChatColor.GREEN + "Updated to 1.8 by: " + ChatColor.YELLOW + "DartCZ");
-	sender.sendMessage(ChatColor.GREEN + "Currently maintained by: " + ChatColor.YELLOW + "Zrips");
-	String names = null;
-	for (String auth : plugin.getAuthors()) {
-	    if (names == null)
-		names = auth;
-	    else
-		names = names + ", " + auth;
-	}
-	sender.sendMessage(ChatColor.GREEN + "Authors: " + ChatColor.YELLOW + names);
-	sender.sendMessage(ChatColor.DARK_AQUA + "For a command list, and help, see the wiki:");
-	sender.sendMessage(ChatColor.GREEN + "https://github.com/bekvon/Residence/wiki");
-	sender.sendMessage(ChatColor.AQUA + "Visit the Spigot Resource page at:");
-	sender.sendMessage(ChatColor.BLUE + "https://www.spigotmc.org/resources/residence.11480/");
-	sender.sendMessage(ChatColor.GRAY + "------------------------------------");
-	return true;
+
+        final String version = plugin.getDescription().getVersion();
+        String build = null;
+
+        String[] split = Bukkit.getVersion().split("-");
+        final String serverType = split.length > 1 ? split[1] : split[0];
+
+        try {
+            if (serverType.equalsIgnoreCase("Paper") || serverType.equalsIgnoreCase("purpur") || Version.isPaper()) {
+                if (Version.isCurrentEqualOrHigher(Version.v1_20_R4))
+                    build = Bukkit.getVersion().split("-")[1].split(" ")[0];
+                else
+                    build = Bukkit.getVersion().split("-")[2].split(" ")[0];
+            } else if (Version.isSpigot())
+                build = Bukkit.getVersion().split("-", 2)[0];
+            Integer.parseInt(build);
+        } catch (Throwable e) {
+            build = null;
+        }
+
+        final String buildVersion = build == null ? "" : "(" + build + ")";
+        Plugin CMILib = Bukkit.getPluginManager().getPlugin("CMILib");
+        String CMILibversion = null;
+        if (CMILib != null) {
+            CMILibversion = CMILib.getDescription().getVersion();
+        }
+        String javaVersion = System.getProperty("java.version");
+
+        LC.info_Spliter.sendMessage(sender);
+
+        CMIMessages.sendMessage(sender, "&fResidence version: &7" + version);
+        CMIMessages.sendMessage(sender, "&fCMILib version: &7" + CMILibversion);
+        if (build != null)
+            CMIMessages.sendMessage(sender, "&fServer version: &7" + CMIText.firstToUpperCase(Version.getPlatform().toString()) + buildVersion);
+        CMIMessages.sendMessage(sender, "&fJava version: &7" + javaVersion);
+
+        LC.info_Spliter.sendMessage(sender);
+
+        return true;
     }
 
     @Override
     public void getLocale() {
-	ConfigReader c = Residence.getInstance().getLocaleManager().getLocaleConfig();
-	c.get("Description", "how residence version");
-	c.get("Info", Arrays.asList("&eUsage: &6/res version"));
+        ConfigReader c = Residence.getInstance().getLocaleManager().getLocaleConfig();
+        c.get("Description", "how residence version");
+        c.get("Info", Arrays.asList("&eUsage: &6/res version"));
     }
 }
