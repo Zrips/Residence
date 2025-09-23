3bad5e104baea9f3043dcd01cc1360f5d8f29308
Zrips <zripas@gmail.com>
2025-09-23 15:58:00 +0300

    Blocking vehicle damage based on destroy flag

--- src/main/java/com/bekvon/bukkit/residence/listeners/ResidencePlayerListener1_14.java
+++ src/main/java/com/bekvon/bukkit/residence/listeners/ResidencePlayerListener1_14.java
@@ -2,11 +2,13 @@ package com.bekvon.bukkit.residence.listeners;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.player.PlayerTakeLecternBookEvent;
+import org.bukkit.event.vehicle.VehicleDamageEvent;
 
 import com.bekvon.bukkit.residence.Residence;
 import com.bekvon.bukkit.residence.containers.Flags;
@@ -14,6 +16,8 @@ import com.bekvon.bukkit.residence.containers.ResAdmin;
 import com.bekvon.bukkit.residence.containers.lm;
 import com.bekvon.bukkit.residence.protection.FlagPermissions;
 import com.bekvon.bukkit.residence.protection.FlagPermissions.FlagCombo;
+
+import net.Zrips.CMILib.Logs.CMIDebug;
 
 public class ResidencePlayerListener1_14 implements Listener {
 
@@ -60,5 +64,33 @@ public class ResidencePlayerListener1_14 implements Listener {
             return;
 
         event.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onVehicleDamage(VehicleDamageEvent event) {
+        // Disabling listener if flag disabled globally
+        if (!Flags.destroy.isGlobalyEnabled())
+            return;
+        // disabling event on world
+        if (plugin.isDisabledWorldListener(event.getVehicle().getWorld()))
+            return;
+
+        if (event.isCancelled())
+            return;
+
+        Entity attacker = event.getAttacker();
+        if (attacker instanceof Player) {
+            if (!FlagPermissions.has(event.getVehicle().getLocation(), (Player) attacker, Flags.destroy, FlagCombo.OnlyFalse))
+                return;
+        } else {
+            if (!FlagPermissions.has(event.getVehicle().getLocation(), Flags.destroy, FlagCombo.OnlyFalse))
+                return;
+        }
+
+        event.setCancelled(true);
+
+        if (attacker instanceof Player)
+            lm.Flag_Deny.sendMessage((Player) attacker, Flags.destroy);
+
     }
 }
