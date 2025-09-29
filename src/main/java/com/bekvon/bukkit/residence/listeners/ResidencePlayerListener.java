a87675898ad7eaa1b4e68dac328b9e7d88422d82
AO <135555687+CatTeaA@users.noreply.github.com>
2025-09-29 17:09:27 +0800

    Make BONE_MEAL not consumed when its interaction is intercepted

--- src/main/java/com/bekvon/bukkit/residence/listeners/ResidencePlayerListener.java
+++ src/main/java/com/bekvon/bukkit/residence/listeners/ResidencePlayerListener.java
@@ -1451,26 +1451,12 @@ public class ResidencePlayerListener implements Listener {
 
             CMIMaterial blockM = CMIMaterial.get(block.getType());
 
-            if (heldItem.isDye() || heldItem.equals(CMIMaterial.GLOW_INK_SAC)) {
-
-                if (heldItem.equals(CMIMaterial.BONE_MEAL) &&
-                    (blockM == CMIMaterial.GRASS_BLOCK ||
-                        blockM == CMIMaterial.GRASS ||
-                        blockM == CMIMaterial.SHORT_GRASS ||
-                        blockM == CMIMaterial.TALL_GRASS ||
-                        blockM == CMIMaterial.TALL_SEAGRASS ||
-                        blockM == CMIMaterial.MOSS_BLOCK ||
-                        blockM == CMIMaterial.BIG_DRIPLEAF_STEM ||
-                        blockM == CMIMaterial.BIG_DRIPLEAF ||
-                        blockM == CMIMaterial.SMALL_DRIPLEAF ||
-                        blockM.isSapling()) ||
-                    heldItem == CMIMaterial.COCOA_BEANS && blockM == CMIMaterial.JUNGLE_WOOD) {
-                    FlagPermissions tperms = FlagPermissions.getPerms(block.getRelative(event.getBlockFace()).getLocation(), player);
-                    if (!tperms.playerHas(player, Flags.build, true)) {
-                        lm.Flag_Deny.sendMessage(player, Flags.build);
-                        event.setCancelled(true);
-                        return;
-                    }
+            if (heldItem.equals(CMIMaterial.BONE_MEAL)) {
+                FlagPermissions tperms = FlagPermissions.getPerms(block.getRelative(event.getBlockFace()).getLocation(), player);
+                if (!tperms.playerHas(player, Flags.build, true)) {
+                    lm.Flag_Deny.sendMessage(player, Flags.build);
+                    event.setCancelled(true);
+                    return;
                 }
             }
             if (heldItem.equals(CMIMaterial.ARMOR_STAND) || heldItem.isBoat()) {
@@ -2913,3 +2899,4 @@ public class ResidencePlayerListener implements Listener {
         return playerTempData.getCurrentResidence(uuid);
     }
 }
+
