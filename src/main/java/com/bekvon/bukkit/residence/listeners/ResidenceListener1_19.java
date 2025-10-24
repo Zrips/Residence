b51816a455a6d8d3c8c45efc285bb5fd8ccf3347
AO <135555687+CatTeaA@users.noreply.github.com>
2025-10-24 18:30:01 +0800

    Fix hopper stealing from edgeResidence container

--- src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener1_19.java
+++ src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener1_19.java
@@ -1,13 +1,17 @@
 package com.bekvon.bukkit.residence.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockSpreadEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import com.bekvon.bukkit.residence.Residence;
@@ -81,5 +85,49 @@ public class ResidenceListener1_19 implements Listener {
         if (!perms.has(Flags.skulk, true)) {
             event.setCancelled(true);
         }
+    }
+
+    private void breakHopper(Inventory hopperInventory) {
+        // delay 1 tick break, ensure after event cancel
+        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+            Location hopperLoc = hopperInventory.getLocation();
+            if (hopperLoc == null)
+                return;
+            Block block = hopperLoc.getBlock();
+            // only hopper
+            if (block == null || !(block.getType().equals(Material.HOPPER)))
+                return;
+            block.breakNaturally();
+        }, 1);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onHopperCrossRes(InventoryMoveItemEvent event) {
+
+        Inventory source = event.getSource();
+        Inventory dest = event.getDestination();
+        if (source == null || dest == null)
+            return;
+
+        ClaimedResidence sourceRes = ClaimedResidence.getByLoc(source.getLocation());
+        ClaimedResidence destRes = ClaimedResidence.getByLoc(dest.getLocation());
+
+        // source & dest not in Same Residence
+        if (sourceRes != null && destRes != null && !sourceRes.equals(destRes)) {
+            event.setCancelled(true);
+            return;
+        }
+        // source in Res, dest not in Res
+        if (sourceRes != null && destRes == null) {
+            event.setCancelled(true);
+            breakHopper(dest);
+            return;
+        }
+        // dest in Res, source not in Res
+        if (sourceRes == null && destRes != null) {
+            event.setCancelled(true);
+            breakHopper(source);
+        }
+        // ignore source & dest not in Res
     }
 }
