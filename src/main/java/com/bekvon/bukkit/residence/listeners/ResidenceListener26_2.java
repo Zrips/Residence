c414d7b62ecd1a8f916bd084f5cca50ec03d37e4
CatTeaA <1615856447@qq.com>
2026-08-22 17:30:19 +0800

    fix SulfurCubeBucket bypass protection

--- src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener26_2.java
+++ src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener26_2.java
@@ -1,13 +1,17 @@
 package com.bekvon.bukkit.residence.listeners;
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.SulfurCube;
+import org.bukkit.event.Event.Result;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.EquipmentSlot;
 
@@ -57,6 +61,43 @@ public class ResidenceListener26_2 implements Listener {
             return;
         }
         lm.Flag_Deny.sendMessage(player, Flags.ignite);
+        event.setCancelled(true);
+
+    }
+
+    // fix https://github.com/PaperMC/Paper/issues/14149
+    @EventHandler(priority = EventPriority.LOWEST) // Do not use (ignoreCancelled = true)
+    public void onPlayerSulfurCubeBucketEmpty(PlayerInteractEvent event) {
+        if (event.useItemInHand() == Result.DENY) {
+            return;
+        }
+        // Disabling listener if flag disabled globally
+        if (!Flags.build.isGlobalyEnabled()) {
+            return;
+        }
+        Block block= event.getClickedBlock();
+        if (block == null) {
+            return;
+        }
+        // disabling event on world
+        if (plugin.isDisabledWorldListener(block.getWorld())) {
+            return;
+        }
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+        if (event.getItem() == null || event.getItem().getType() != Material.SULFUR_CUBE_BUCKET) {
+            return;
+        }
+        Player player = event.getPlayer();
+        if (ResAdmin.isResAdmin(player)) {
+            return;
+        }
+        if (FlagPermissions.has(block.getRelative(event.getBlockFace()).getLocation(),  player, Flags.build, true)) {
+            return;
+        }
+
+        lm.Flag_Deny.sendMessage(player, Flags.build);
         event.setCancelled(true);
 
     }
